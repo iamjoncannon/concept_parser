a1e55f047a3163819be8727fdb24e43dbd098281
jon cannon <iamjoncannon@gmail.com>
2019-04-03 13:20:29 -0400

    changed main

--- app/main.js
+++ app/main.js
@@ -1,10 +1,15 @@
-import React from 'react'
-import {render} from 'react-dom'
-import { Provider} from 'react-redux'
-import store from './store'
-import Root from './components/root'
+import React, { useState } from 'react'
+import { render } from 'react-dom'
+// import { Provider} from 'react-redux'
+// import store from './store'
+// import Root from './components/root'
 import { ForceGraph3D } from 'react-force-graph';
 import AllCampus from './components/all_campus'
+
+import Wrapper from './components/wrapper'
+
+// console.dir(useState)
+
 // render(
 //   <Provider store={store}>
 //     <Root />
@@ -13,6 +18,8 @@ import AllCampus from './components/all_campus'
 // )
 
 render(
-  <AllCampus />, 
+	<Wrapper>
+ 	 <AllCampus />
+  </Wrapper>,
   document.getElementById('main')
 );
