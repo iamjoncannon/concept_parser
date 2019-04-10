3166b7d44a2daac5b58ea51f8815862e2c1624e2
jon cannon <iamjoncannon@gmail.com>
2019-04-10 19:55:38 -0400

    working on database url again

--- server/db/database.js
+++ server/db/database.js
@@ -5,8 +5,10 @@ const Sequelize = require('sequelize')
 const pkg = require('../../package.json')
 
 console.log(chalk.yellow('Opening database connection'))
+console.log("process.env.DATABASE_URL = ", process.env.DATABASE_URL)
 
-const db = new Sequelize(process.env.DATABASE_URL || `postgres://localhost:5432/${pkg.name}`, {
+
+const db = new Sequelize(process.env.DATABASE_URL, {
   logging: false // so we don't see all the SQL queries getting made
 })
 
